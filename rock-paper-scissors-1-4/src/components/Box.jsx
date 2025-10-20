a47a743481f9d2fa23dce71f8c6f99efec46ed70
eo <115367925+aromasa@users.noreply.github.com>
2025-10-20 21:10:04 +0900

    Add files via upload
    aaaa

--- rock-paper-scissors-1-4/src/components/Box.jsx
+++ rock-paper-scissors-1-4/src/components/Box.jsx
@@ -4,11 +4,11 @@ export const Box = (props) => {
   return (
     <div className="box">
       <h1>{props.title}</h1>
-
-      {/* <img src={props.item && props.item.img} /> */}
-
+      
       <div className="box-img">
-        <img src={props.item?.img} />
+      {/* <img src={props.item && props.item.img} /> */}
+      {/* <img src={props.item?.img} /> */}
+        <img src={props.item?.img || 'https://i.namu.wiki/i/J9A5vBE6BuFBd09_MojiggkfWoci1CuuIjNf19nuSnxmfqmx4oYePJh0FWVzCmc5AIHxVoi2QWF8G5yIs_iwRA.webp'} />
       </div>
       <h2>WIN</h2>
     </div>
